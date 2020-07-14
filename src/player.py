989fdcf888cc2d14454dd341355149621175c766
Kronykus <MoreheadSteve@gmail.com>
2020-07-14 00:02:07 -0500

    forked/branched/basic outline

--- src/player.py
+++ src/player.py
@@ -1,2 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__( self, name, room="outside" ):
+        self.name         = name
+        self.current_room = room
+
+    def move_to( self, room ):
+        self.current_room = room
+
+    def get_room( self ):
+        return self.current_room
